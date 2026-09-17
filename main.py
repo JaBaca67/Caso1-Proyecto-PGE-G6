490aef5e4a2c5cde1c3d96dfdc3fa7a6a45722aa
JOSE ALFREDO BACA MORENO <jabaca@uamv.edu.ni>
2026-09-17 14:29:13 -0600

    Bases de la main y bucle principal del menu.

--- main.py
+++ main.py
@@ -4,3 +4,53 @@ import condiciones
 import entrada
 import resultados
 import validaciones
+
+
+def showMenu():
+    print("╔══════════════════════════════════════════════════════════════════════╗")
+    print("║               SISTEMA DE GESTIÓN DE VENTAS Y COMISIONES              ║")
+    print("╚══════════════════════════════════════════════════════════════════════╝")
+
+    print("1. Registrar y calcular comisiones de vendedor")
+    print("2. Ver criterios de comisión y metas de bono")
+    print("3. Salir del programa")
+    #Nota estas opciciones del menu son temporales. Aun se pueden organizar mejor. Más tarde lo hago 😎
+
+    print("═════════════════════════════════════════════════════════════════════")
+
+def systemExecution():
+
+    option = ""
+
+    #bucle del menú principal
+    while option != 3:
+        showMenu()
+        option = input("Selccione una opción (1-3): ").strip()#Selección del menu.
+        print()#Espacio en blanco
+
+        if option == "1": 
+            print("[Módulo de cálculo]")
+            #Aun en desarrollo
+        
+        elif option == "2":
+            print("[Criterios de comisión y bono]")
+            #Aun en desarollo
+
+            #Siguiente prints son temporales, aun puede cambiar a futuro.
+            print("• Ventas < $5,000      -> 5% de comisión")
+            print("• Ventas $5,000-$9,999 -> 10% de comisión")
+            print("• Ventas >= $10,000    -> 15% de comisión + Bono adicional\n")
+
+        elif option == "3":
+            print("Saliendo del programa...")
+
+        else:
+            print("Opción invalida. Porfavor, ingrese un número del 1 - 3.")
+
+        if option != "3":
+            input("Presione enter para volver al menú principal...")
+            print("\n" * 2) #Esto sirve para dejar mas espacio desues den input.
+
+        
+
+        
