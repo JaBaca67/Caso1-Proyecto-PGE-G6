9e54b6220f85b7cdfd73ffac1cc71f147df1acde
Maykel Cruz <maykelcruz@uamv.edu.ni>
2026-09-16 20:30:02 -0600

    Explicacion breve de la sinergia de cada parametro

--- calculos.py
+++ calculos.py
@@ -1,5 +1,12 @@
 def calculate_Comission(sales, percentage=0.5):
     return sales * percentage
 
+
 def calculate_Total_Incomes(salary, comission, bonus=0):
     return salary + comission + bonus
+
+
+## El parametro bonus se vera afectado por una funcion que determine el bono segun las ventas en el modulo condiciones, 
+# de manera que sera reasignado en main con el valor determinado y finalmente se sumara en esta funcion
+## Comission sera el valor de la primera funcion de este modulo, calculate_Comission, 
+# valor el cual se le asignara en el archivo main y finalmente hara su trabajo en la segunda funcion; calculate_Total_Incomes
