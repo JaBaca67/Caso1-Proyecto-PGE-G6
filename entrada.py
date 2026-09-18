7f256818d0f46d9a533c35c16377ee78f1914ec4
HUMBERTO RENE RUIZ RODRIGUEZ <hrruiz@uamv.edu.ni>
2026-09-17 18:48:32 -0600

    Quite el salario base que no pide el ejercicio y agregue las comisiones

--- entrada.py
+++ entrada.py
@@ -1,10 +1,8 @@
 def readInfo():
     sellerName = input("Ingrese su nombre: ")
-    baseSalary = float(input("Ingrese su sueldo base: "))
     sales = float(input("Ingrese sus ventas: "))
     return {
         "sellerName": sellerName,
-        "baseSalary": baseSalary,
         "sales": sales
     }
 
--- resultados.py
+++ resultados.py
@@ -3,18 +3,17 @@ from condiciones import determine_Bonus, determine_Percentage
 
 def showData(sellerData):
     sellerName = sellerData["sellerName"]
-    baseSalary = sellerData["baseSalary"]
     sales = sellerData["sales"]
 
     percentage = determine_Percentage(sales)
     bonus = determine_Bonus(sales)
     comission = calculate_Comission(sales, percentage)
-    total = calculate_Total_Incomes(baseSalary, comission, bonus )
+    total = calculate_Total_Incomes(comission, bonus )
 
     print("=== DATOS DEL VENDEDOR ===")
     print(f"Vendedor: {sellerName}")
-    print(f"Salario base: {baseSalary}")
     print(f"Ventas: {sales}")
     print(f"Porcentaje: {percentage}")
+    print(f"Comisiones: {comission}")
     print(f"Bonus: {bonus}")
     print(f"Total: {total}")
