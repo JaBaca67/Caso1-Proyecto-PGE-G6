bfbf4a76cb97b2f16b378958c7e2e56527009130
HUMBERTO RENE RUIZ RODRIGUEZ <hrruiz@uamv.edu.ni>
2026-09-17 18:36:45 -0600

    Importe calculos y condiciones para mostrar la data en los datos del vendedor

--- resultados.py
+++ resultados.py
@@ -1,9 +1,20 @@
+from calculos import calculate_Comission, calculate_Total_Incomes, calculate_Total_Sales
+from condiciones import determine_Bonus, determine_Percentage
+
 def showData(sellerData):
     sellerName = sellerData["sellerName"]
     baseSalary = sellerData["baseSalary"]
     sales = sellerData["sales"]
 
+    percentage = determine_Percentage(sales)
+    bonus = determine_Bonus(sales)
+    comission = calculate_Comission(sales, percentage)
+    total = calculate_Total_Incomes(baseSalary, comission, bonus )
+
     print("=== DATOS DEL VENDEDOR ===")
     print(f"Vendedor: {sellerName}")
     print(f"Salario base: {baseSalary}")
     print(f"Ventas: {sales}")
+    print(f"Porcentaje: {percentage}")
+    print(f"Bonus: {bonus}")
+    print(f"Total: {total}")
